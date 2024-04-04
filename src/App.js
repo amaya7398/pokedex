1c3dbdc25847c85fd17e0f871d8180ab2b812b88
Daniel Amaya <daniel_amaya7398@hotmail.com>
2024-04-03 23:54:07 -0600

    feat: Pokedex React lista
    V1, se espera una sección con un grafico de telaraña con sus stats, sus ataques o información adicional del pokemon escogido.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
+
+  const [pokes, setPokes] = useState([])
+  const [sprite, setSprite] = useState('')
+
+  const fetchData = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(res => res.json())
+      .then(pokes => setPokes(pokes.results))
+  }
+
+  const displayAllPokemons = () => {
+    return <ul className='pokedex-info-pokes'>
+      {
+        pokes.map((pk, idx) =>
+          <li key={idx} onClick={displayPokemon} >{pk.name}</li>)
+      }
+    </ul>
+  }
+
+  const displayPokemon = async (event) => {
+    const namePoke = event.target.innerText
+    const spritesLink = await getPokemonByName(namePoke)
+    setSprite(spritesLink)
+  }
+
+  const getPokemonByName = async (name) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    const data = await response.json()
+    const sprite = data.sprites.other.showdown.front_default
+    return sprite
+  }
+
+  const spritePokemon = () => {
+    const spriteSrc = sprite === ''
+      ? "https://img.itch.zone/aW1nLzk4ODIxOTQuZ2lm/original/NigZHO.gif"
+      : sprite
+    return <img src={spriteSrc} alt="pokemon sprite" srcSet="" />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pokedex">
+      <img className='pokedex-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" alt="" srcSet="" />
+      <div className="pokedex-display">{spritePokemon()}</div>
+      <button className='pokedex-button' onClick={fetchData}>Catch all pokemon</button>
+      <div className="pokedex-info">
+        {displayAllPokemons()}
+      </div>
+      <div className="pokedex-buttons-selection"></div>
+
     </div>
   );
 }
