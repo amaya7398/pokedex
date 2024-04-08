590b1a0cf4fa1d1e50c47c506d03d8eb2a503151
Daniel Amaya <daniel_amaya7398@hotmail.com>
2024-04-08 17:56:30 -0600

    feat: Information of an specific pokemon

--- src/components/pokedex/pokedex.js
+++ src/components/pokedex/pokedex.js
@@ -1,15 +1,17 @@
+import './pokedex.css'
+import { getColorType } from '../pokemon-type-colours'
 import { useState } from 'react';
 
 export default function Pokedex() {
-    const defaultPokemon = { name: "---", types: [], abilities: "---", sprite: "https://img.itch.zone/aW1nLzk4ODIxOTQuZ2lm/original/NigZHO.gif" }
+    const defaultPokemon = { id: 0, name: "---", types: [], stats: [], weight: 0, height: 0, abilities: "---", sprite: "https://img.itch.zone/aW1nLzk4ODIxOTQuZ2lm/original/NigZHO.gif" }
 
     const [pokes, setPokes] = useState([])
     const [pokemon, setPokemon] = useState(defaultPokemon)
     const [statusDisplay, setStatusDisplay] = useState(false)
 
     const fetchData = () => {
-        setStatusDisplay(false)
         setPokemon(defaultPokemon)
+        setStatusDisplay(false)
         fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
             .then(res => res.json())
             .then(pokes => setPokes(pokes.results))
@@ -17,21 +19,28 @@ export default function Pokedex() {
 
     const displayPokemon = async (event) => {
         const namePoke = event.target.innerText
-        const { name, sprite, types } = await getPokemonByName(namePoke)
+        const { id, name, sprite, types, stats, height, weight } = await getPokemonByName(namePoke)
 
-        // TODO: Fetch the pokemon data from the API
-        setPokemon({ ...pokemon, sprite, name, types })
+        setPokemon({ ...pokemon, id, sprite, name, types, stats, height, weight })
         setStatusDisplay(true)
+        console.log({ pokemon })
     }
 
     const getPokemonByName = async (name) => {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
         const data = await response.json()
+
         console.log(data)
+
+        const id = data.id
         const spriteLink = data.sprites.other.showdown.front_default
         const types = data.types.map(type => type.type.name)
-        console.log(types)
-        return { name, sprite: spriteLink, types }
+        const weight = data.weight
+        const height = data.height
+        const stats = data.stats
+        // [] map( x => x.stat.name x.base_stat)
+
+        return { id, name, sprite: spriteLink, types, stats, height, weight }
     }
 
     const getStatusDisplay = () => {
@@ -66,7 +75,16 @@ export default function Pokedex() {
                 <img className='pokedex-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" alt="" srcSet="" />
 
                 <div className={"info-pokemon pokedex-display " + getStatusDisplay()}>
-                    {/* <img src={pokemon.sprite} alt="pokemon sprite" srcSet="" /> */}
+                    <div className="pokemonName">{pokemon.name}</div>
+                    <div className="pokemonTypes">
+                        {
+                            pokemon.types.map((type, idx) =>
+                                <div key={idx} className="type color" style={{ backgroundColor: getColorType(type) }}> {type} </div>
+                            )
+                        }
+                    </div>
+                    <div className="pokemonWeight">{"Weight " + pokemon.weight}</div>
+                    <div className="pokemonHeight">{"Height " + pokemon.height}</div>
                 </div>
 
                 <div className={"grafico-stats pokedex-display " + getStatusDisplay()}>
@@ -75,16 +93,6 @@ export default function Pokedex() {
                     </div>
                 </div>
 
-                <div className="pokemonName">{pokemon.name}</div>
-                {/* <div className="pokemonType">{pokemon.type}</div> */}
-                <div className="pokemonAbilities">
-                    {
-                        pokemon.types.map((type, idx) =>
-                            <div key={idx} className="ability">{type}</div>
-                        )
-                    }
-                </div>
-                <div className="pokemonAbilities">{pokemon.abilities}</div>
             </div>
 
         </div>
